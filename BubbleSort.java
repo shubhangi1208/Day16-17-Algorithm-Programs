a0e79cf80a8735728bb9de70fe6acbaca743a12c
shubhangi1208 <96242483+shubhangi1208@users.noreply.github.com>
2022-01-21 13:20:36 +0530

    ADDED: Bubble Sort Program

---
+++ BubbleSort.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+
+public class BubbleSort {
+    //array is traversed from first element to last element. Here, current element is compared with the next element.
+    // If current element is greater than the next element, it is swapped.
+    void bubbleSort( int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+    }
+
+    public static void main(String[] args) {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
